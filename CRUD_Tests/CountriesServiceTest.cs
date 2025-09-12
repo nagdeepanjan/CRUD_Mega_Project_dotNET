11e25fdeeeebe91baf86fb38d8e5a1d019db3eec
nagdeepanjan <mail@deepanjannag.com>
2025-09-12 09:52:00 -0400

    CountryAdd

---
+++ CRUD_Tests/CountriesServiceTest.cs
@@ -0,0 +1,78 @@
+﻿using ServiceContracts;
+using ServiceContracts.DTO;
+using Services;
+
+namespace CRUD_Tests
+{
+    public class CountriesServiceTest
+    {
+        private readonly ICountriesService _countriesService;
+
+        public CountriesServiceTest()
+        {
+            _countriesService = new CountriesService();
+
+        }
+
+        //When CountryAddRequest is null, throw ArgumentNullException
+        [Fact]
+        public void AddCountry_NullCountry()
+        {
+            //Arrange
+            CountryAddRequest? request = null;
+
+            //Assert
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                //Act
+                _countriesService.AddCountry(request);
+            });
+        }
+
+        //When CountryName is null, throw ArgumentException
+        [Fact]
+        public void AddCountry_CountryNameIsNull()
+        {
+            //Arrange
+            CountryAddRequest? request = new CountryAddRequest { CountryName = null };
+
+            //Assert
+            Assert.Throws<ArgumentException>(() =>
+            {
+                //Act
+                _countriesService.AddCountry(request);
+            });
+        }
+        //When CountryName is duplicate, throw ArgumentException
+        [Fact]
+        public void AddCountry_DuplicateCountryName()
+        {
+            //Arrange
+            CountryAddRequest? request = new CountryAddRequest { CountryName = "Alpha" };
+            CountryAddRequest? request2 = new CountryAddRequest { CountryName = "Alpha" };
+
+            //Assert
+            Assert.Throws<ArgumentException>(() =>
+            {
+                //Act
+                _countriesService.AddCountry(request);
+                _countriesService.AddCountry(request2);
+            });
+        }
+
+
+        //When proper CountryName is supplied, it should be added to the list of countries.
+        [Fact]
+        public void AddCountry_OK()
+        {
+            //Arrange
+            CountryAddRequest? request = new CountryAddRequest { CountryName = "Sweden" };
+
+            //Act
+            CountryResponse response = _countriesService.AddCountry(request);
+
+            //Assert
+            Assert.True(response.CountryID != Guid.Empty);
+        }
+    }
+}
