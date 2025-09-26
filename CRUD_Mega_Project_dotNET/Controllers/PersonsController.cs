0ee62504667e31730dd2a906f8bb37b2f9123ec1
nagdeepanjan <mail@deepanjannag.com>
2025-09-26 08:19:16 -0400

    Add PersonsController and basic Razor views
    Updated the project file to include references to `Entities`, `ServiceContracts`, and `Services` projects, establishing dependencies. Added `PersonsController` with an `Index` action method to handle `/persons/index` and `/` routes. Created `Index.cshtml` view with a title and header. Introduced `_Layout.cshtml` for a consistent layout, including navigation and styling. Updated `_ViewImports.cshtml` to include necessary namespaces. Added `_ViewStart.cshtml` to set the default layout for views.

---
+++ CRUD_Mega_Project_dotNET/Controllers/PersonsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_Mega_Project_dotNET.Controllers
+{
+    public class PersonsController : Controller
+    {
+        [Route("/persons/index")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
